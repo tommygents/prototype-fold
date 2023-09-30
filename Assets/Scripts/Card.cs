dc8a000a9af2dfa86b0107268a1f10fa0ddb1d83
tommygents <tmaranges+gh@gmail.com>
2023-09-29 22:27:05 -0400

    began wiriting code, set up 3d scene

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    public GameObject topHalfCard;
+    public GameObject bottomHalfCard;
+    public CardManager cardManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
